4f0ec3c1e6294af6dd76bab2fd0c543a8ef9670b
Ruslan <templar1128@Hotmail.com>
2022-11-12 19:39:02 +0200

    Add URLConvertible protocol

--- Sources/NetRunner/NetworkRequest.swift
+++ Sources/NetRunner/NetworkRequest.swift
@@ -4,13 +4,27 @@ import Foundation
 typealias Parameters = [String:Any]
 typealias Headers = [String:String]
 
-protocol NetworkRequest {
+protocol URLRequestConvertible {
+    func makeURLRequest() throws -> URLRequest
+}
+
+protocol NetworkRequest: URLRequestConvertible {
     var url: String { get }
     var method: HTTPMethod { get }
     var endpoint: Endpoint { get }
     var parameters: Parameters? { get set }
     var headers: Headers { get set }
     var body: Data? { get set }
+}
+
+extension NetworkRequest {
+    func makeURLRequest() throws -> URLRequest {
+        guard let url = URL(string: [url, endpoint.path].joined()) else {
+            throw NetworkError.badURL
+        }
+        
+        return URLRequest(url: url)
+    }
 }
 
 protocol Endpoint {
