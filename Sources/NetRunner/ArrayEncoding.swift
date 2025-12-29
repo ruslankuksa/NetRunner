16bb6859d932629ebacb5e63d2a59e5075694670
Ruslan Kuksa <templar1128@hotmail.com>
2025-12-29 12:33:43 +0100

    Added ArrayEncoding

---
+++ Sources/NetRunner/ArrayEncoding.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+/// Defines how array parameters should be encoded in URL query strings
+public enum ArrayEncoding {
+    /// Encodes arrays with brackets: `key[]=1&key[]=2&key[]=3`
+    /// This is the most common format used by Rails and similar frameworks
+    case brackets
+
+    /// Encodes arrays without brackets: `key=1&key=2&key=3`
+    case noBrackets
+
+    /// Encodes arrays as comma-separated values: `key=1,2,3`
+    case commaSeparated
+}
--- Sources/NetRunner/NetworkRequest.swift
+++ Sources/NetRunner/NetworkRequest.swift
@@ -11,35 +11,38 @@ public protocol NetworkRequest {
     var headers: HTTPHeaders? { get set }
     var parameters: Parameters? { get }
     var httpBody: Encodable? { get }
-    
+
     var decoder: JSONDecoder { get }
     var encoder: JSONEncoder { get }
+    var arrayEncoding: ArrayEncoding { get }
 }
 
 public extension NetworkRequest {
-    
+
     var decoder: JSONDecoder {
         return .init()
     }
-    
+
     var encoder: JSONEncoder {
         return .init()
     }
-    
+
+    var arrayEncoding: ArrayEncoding {
+        return .brackets
+    }
+
     func asURLRequest() throws -> URLRequest {
         let urlPath = [url, endpoint.path].joined()
         var components = URLComponents(string: urlPath)
-        
+
         if let parameters = parameters, !parameters.isEmpty {
-            components?.queryItems = parameters.map {
-                return URLQueryItem(name: $0, value: "\($1)")
-            }
+            components?.queryItems = buildQueryItems(from: parameters, encoding: arrayEncoding)
         }
-        
+
         guard let url = components?.url else {
             throw NetworkError.badURL
         }
-        
+
         var request = URLRequest(url: url, httpMethod: method, headers: headers)
         if let httpBody {
             if method != .get {
@@ -48,7 +51,40 @@ public extension NetworkRequest {
                 throw NetworkError.notAllowedRequest
             }
         }
-        
+
         return request
+    }
+
+    private func buildQueryItems(from parameters: Parameters, encoding: ArrayEncoding) -> [URLQueryItem] {
+        var queryItems: [URLQueryItem] = []
+
+        for (key, value) in parameters {
+            if let array = value as? [Any] {
+                // Handle array values based on encoding type
+                switch encoding {
+                case .brackets:
+                    // key[]=1&key[]=2&key[]=3
+                    for item in array {
+                        queryItems.append(URLQueryItem(name: key, value: "\(item)"))
+                    }
+                case .noBrackets:
+                    // Remove brackets if present in key
+                    let cleanKey = key.replacingOccurrences(of: "[]", with: "")
+                    for item in array {
+                        queryItems.append(URLQueryItem(name: cleanKey, value: "\(item)"))
+                    }
+                case .commaSeparated:
+                    // key=1,2,3
+                    let cleanKey = key.replacingOccurrences(of: "[]", with: "")
+                    let commaSeparatedValue = array.map { "\($0)" }.joined(separator: ",")
+                    queryItems.append(URLQueryItem(name: cleanKey, value: commaSeparatedValue))
+                }
+            } else {
+                // Handle single values normally
+                queryItems.append(URLQueryItem(name: key, value: "\(value)"))
+            }
+        }
+
+        return queryItems
     }
 }
